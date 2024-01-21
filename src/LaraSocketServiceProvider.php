c8aa9f3aa9bd123ee6052c51d85e56ccf17a4a2e
Akın <akincand@yandex.com>
2024-01-22 00:19:56 +0300

    .

---
+++ src/LaraSocketServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AkincanD\LaraSocket;
+
+use Illuminate\Auth\Events\Logout;
+use Illuminate\Session\SessionManager;
+use Illuminate\Support\ServiceProvider;
+
+class LaraSocketServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('larasocket', 'AkincanD\LaraSocket\LaraSocket');
+
+        $config = __DIR__ . '/../config/larasocket.php';
+        $this->mergeConfigFrom($config, 'larasocket');
+
+        $this->publishes([__DIR__ . '/../config/larasocket.php' => config_path('larasocket.php')], 'config');
+    }
+}
