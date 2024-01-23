d52f437ca6203fbe8439387b78533fe5a4c17937
Akın <akincand@yandex.com>
2024-01-23 05:06:32 +0300

    .

--- src/LaraSocketServiceProvider.php
+++ src/LaraSocketServiceProvider.php
@@ -10,36 +10,20 @@ use Illuminate\Support\ServiceProvider;
 class LaraSocketServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->mergeConfigFrom(__DIR__ . '/config/larasocket.php', 'larasocket');
-    }
-
-    /**
      * Bootstrap any application services.
      *
      * @return void
      */
     public function boot()
     {
-        $this->configurePublishing();
-    }
+        $this->publishes([
+            __DIR__ . '/config/larasocket.php' => config_path('larasocket.php'),
+        ]);
 
-    /**
-     * Configure publishing for the package.
-     *
-     * @return void
-     */
-    protected function configurePublishing()
-    {
-        if (function_exists('config_path')) {
-            $this->publishes([
-                __DIR__ . '/config/larasocket.php' => config_path('larasocket.php'),
-            ], 'config');
-        }
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/larasocket.php',
+            'larasocket'
+        );
+
     }
 }
